4bc224e17d0ea174dd4a7d3ddff6e8e6cbb0a208
Sammy Israwi <sammy.israwi@gmail.com>
2016-10-19 16:44:33 -0400

    Initial commit, no data or styling or anything

---
+++ client/main.js
@@ -0,0 +1,4 @@
+import { Template } from 'meteor/templating';
+
+
+import '../imports/ui/body.js';
---
+++ imports/ui/body.js
@@ -0,0 +1,9 @@
+import {Meteor} from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+
+
+import './article.js';
+import './header.js';
+import './footer.js';
+
+import './body.html';
